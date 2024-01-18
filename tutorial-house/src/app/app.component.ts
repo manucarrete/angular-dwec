f86933d243062be45d893f2fe9880831063583f9
manucarrete <146010216+manucarrete@users.noreply.github.com>
2024-01-18 19:38:28 +0000

    First commit

---
+++ tutorial-house/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HomeComponent} from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+  title = 'tutorial-house';
+}
